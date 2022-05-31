2169c37032d3b7cfde66952a6c8ac0fed55877bc
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-05-31 18:56:49 -0300

    Added Card links and Css to Home

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import css from "./Cards.module.css";
 import dogsImage from '../Img/Captura-Dogs-App.png'
 import ecommerceImage from '../Img/Captura-ecommerce.png'
+import gitHub from '../Img/github.png'
+import Link from '../Img/link.png'
+
+
 function Cards({ img, name, gitHubLink, deployLink }) {
+    const [icons, setIcons]=useState(false)
+    const handleIcons = ()=>{
+        setIcons(!icons)
+    }
     let image
     if(img === 'ecommerce'){image = ecommerceImage}
     if(img === 'dogs'){image = dogsImage}
   return (
     <div className={css.container} style={{ backgroundImage: `url(${image})` }}>
       <div className={css.layout}>
-        <a className={css.link} target="_blank" href={deployLink}>
-            DeployLink
-        </a>
+        <div className={css.iconContainer} onMouseEnter={()=>handleIcons()} onMouseLeave={()=>handleIcons()} >
+            {icons && <a className={css.link} target="_blank" href={deployLink}>
+                <img className={css.icon} src={Link} alt="Deploy icon" />
+            </a>}
+            
+           {icons && <a className={css.link} target="_blank" href={gitHubLink}>
+                <img className={css.icon} src={gitHub} alt="Github icon" />
+            </a>}
+        </div>
       </div>
       
     </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,50 +1,77 @@
-import React, {useState} from 'react'
-import css from './Home.module.css'
-import img from '../Img/gitgub-redonda.png'
-import Cards from '../Cards/Cards'
-
+import React, { useState } from "react";
+import css from "./Home.module.css";
+import img from "../Img/gitgub-redonda.png";
+import Cards from "../Cards/Cards";
 
 function Home() {
   const [projects] = useState([
-    {img:'ecommerce', name: 'Ecommerce-App', gitHubLink: '', deployLink:'https://clothes-22.vercel.app' },
-    {img: 'dogs', name: 'Dogs-App', gitHubLink: '', deployLink:'https://dogs-lilac-nu.vercel.app' }
-  ])
+    {
+      img: "ecommerce",
+      name: "Ecommerce-App",
+      gitHubLink: "https://github.com/CasanovaRamiro/ecommerce-pf",
+      deployLink: "https://clothes-22.vercel.app",
+    },
+    {
+      img: "dogs",
+      name: "Dogs-App",
+      gitHubLink: "https://github.com/CasanovaRamiro/Dogs",
+      deployLink: "https://dogs-lilac-nu.vercel.app",
+    },
+  ]);
   const [spanish] = useState({
-    subTitle:'Frontend developer viviendo en Buenos Aires, Argentina',
-    description:'Apasionado por el desarrollo web. Aprendizaje constante de nuevas tecnologías. Mas abajo podras ver algunos de mis proyectos.'
-  })
-  const [language, setLanguage] = useState('Es')
-  const handleTranslate = ()=>{
-    if(language === 'Es'){
-      setLanguage('En')
-    }else{setLanguage('Es')}
-  }
+    subTitle: "Frontend developer viviendo en Buenos Aires, Argentina",
+    description:
+      "Apasionado por el desarrollo web. Aprendizaje constante de nuevas tecnologías. Mas abajo podras ver algunos de mis proyectos.",
+  });
+  const [language, setLanguage] = useState("EN");
+  const handleTranslate = () => {
+    if (language === "ES") {
+      setLanguage("EN");
+    } else {
+      setLanguage("ES");
+    }
+  };
   return (
     <div className={css.container}>
+      <div className={css.translateButton}><button onClick={() => handleTranslate()}>{language}</button></div>
       <div className={css.titleImg}>
-      <div className={css.title}><h1>Ramiro Casanova</h1><button onClick={()=>handleTranslate()}>{language}</button></div>
-      <div className={css.img} ><img src={img} alt="Profile Pic" /></div>
+        <div className={css.title}>
+          <div className={css.titleSubTitle}>
+            <h1>Ramiro Casanova</h1>
+            <h4>
+              {language === "EN"
+                ? "Frontend developer based in Buenos Aires, Argentina"
+                : spanish.subTitle}
+            </h4>
+          </div>
+          
+        </div>
+        <div className={css.img}>
+          <img src={img} alt="Profile Pic" />
+        </div>
       </div>
-      <h4>{language === 'Es'?'Frontend developer based in Buenos Aires, Argentina': spanish.subTitle}</h4>
-      <h4>{language === 'Es'?'Passionate about web development. Always learning new technologies. Here you will see some of projects.':spanish.description}</h4>
-      <h2>{language === 'Es'?'Projects':'Proyectos'}</h2>
+      <h4>
+        {language === "EN"
+          ? "Passionate about web development. Always learning new technologies. Here you will see some of projects."
+          : spanish.description}
+      </h4>
+      <h2>{language === "EN" ? "Projects" : "Proyectos"}</h2>
 
-  <div className={css.cardContainer}>   
-    {projects.map(e=>{
-      return(
-        <Cards 
-          key={e.name}
-          img={e.img}
-          name={e.name}
-          gitHubLink={e.gitHubLink}
-          deployLink={e.deployLink}
-        />
-      )
-    }) }
-      
-  </div>  
+      <div className={css.cardContainer}>
+        {projects.map((e) => {
+          return (
+            <Cards
+              key={e.name}
+              img={e.img}
+              name={e.name}
+              gitHubLink={e.gitHubLink}
+              deployLink={e.deployLink}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
