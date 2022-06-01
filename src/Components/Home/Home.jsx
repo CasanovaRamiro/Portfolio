3d20b642f8f268e6067f5feb5d57bf16300398cc
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-06-01 19:55:08 -0300

    Added hover effects an changed font

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import css from "./Home.module.css";
-import img from "../Img/gitgub-redonda.png";
+import img from "../Img/gitgub4.jpeg";
 import Cards from "../Cards/Cards";
 
 function Home() {
@@ -35,6 +35,9 @@ function Home() {
     <div className={css.container}>
       <div className={css.translateButton}><button onClick={() => handleTranslate()}>{language}</button></div>
       <div className={css.titleImg}>
+         <div className={css.img}>
+          <img src={img} alt="Profile Pic" />
+        </div>
         <div className={css.title}>
           <div className={css.titleSubTitle}>
             <h1>Ramiro Casanova</h1>
@@ -44,31 +47,33 @@ function Home() {
                 : spanish.subTitle}
             </h4>
           </div>
-          
         </div>
-        <div className={css.img}>
-          <img src={img} alt="Profile Pic" />
-        </div>
+       
       </div>
-      <h4>
+      <h4 className={css.description} >
         {language === "EN"
           ? "Passionate about web development. Always learning new technologies. Here you will see some of projects."
           : spanish.description}
       </h4>
-      <h2>{language === "EN" ? "Projects" : "Proyectos"}</h2>
+      <div className={css.projects}>
+        <h2>{language === "EN" ? "Projects" : "Proyectos"}</h2>
+        <div className={css.cardContainer}>
+          {projects.map((e) => {
+            return (
+              <Cards
+                key={e.name}
+                img={e.img}
+                name={e.name}
+                gitHubLink={e.gitHubLink}
+                deployLink={e.deployLink}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className={css.technologies}>
+      <h2>{language === "EN" ? "Technologies" : "Tecnologias"}</h2>
 
-      <div className={css.cardContainer}>
-        {projects.map((e) => {
-          return (
-            <Cards
-              key={e.name}
-              img={e.img}
-              name={e.name}
-              gitHubLink={e.gitHubLink}
-              deployLink={e.deployLink}
-            />
-          );
-        })}
       </div>
     </div>
   );
