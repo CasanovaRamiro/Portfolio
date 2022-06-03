839ccb371ddd55edb99e8975757593a2dc74508f
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-06-03 17:57:39 -0300

    Added info functionality on projects

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -2,32 +2,59 @@ import React,{useState} from "react";
 import css from "./Cards.module.css";
 import dogsImage from '../Img/Captura-Dogs-App.png'
 import ecommerceImage from '../Img/Captura-ecommerce.png'
-import gitHub from '../Img/github.png'
-import Link from '../Img/link.png'
+import{FaExternalLinkAlt, } from 'react-icons/fa'
+import { SiGithub} from 'react-icons/si'
+import { BsInfoLg} from 'react-icons/bs'
+import {AiOutlineCloseSquare} from 'react-icons/ai'
 
 
-function Cards({ img, name, gitHubLink, deployLink }) {
+
+
+function Cards({ img, name, gitHubLink, deployLink, infoEspañol, infoEnglish , language}) {
     const [icons, setIcons]=useState(false)
     const handleIcons = ()=>{
         setIcons(!icons)
     }
+    const [infoModal, setInfoModal]=useState(false)
+    const handleInfoModal=()=>{
+      setInfoModal(!infoModal)
+    }
+    
     let image
     if(img === 'ecommerce'){image = ecommerceImage}
     if(img === 'dogs'){image = dogsImage}
   return (
-    <div className={css.container} style={{ backgroundImage: `url(${image})` }}>
-      <div className={css.layout}>
-        <div className={css.iconContainer} onMouseEnter={()=>handleIcons()} onMouseLeave={()=>handleIcons()} >
-            {icons && <a className={css.link} target="_blank" href={deployLink}>
-                <img className={css.icon} src={Link} alt="Deploy icon" />
-            </a>}
-            
-           {icons && <a className={css.link} target="_blank" href={gitHubLink}>
-                <img className={css.icon} src={gitHub} alt="Github icon" />
-            </a>}
+    <div>
+      {infoModal? <div className={css.overlay}><div className={css.overlay} onClick={()=>handleInfoModal()}></div>
+          <div className={css.infoCard}>
+             
+             <div className={css.infoContainer}>
+              <h3>{name}</h3>
+              {language === 'EN'? infoEnglish.map((e, i)=>(<li  key={i}>{e}</li>)):infoEspañol.map((e, i)=>(<li  key={i}>{e}</li>))}
+             </div>
+
+             <span ><AiOutlineCloseSquare size={'30px'} onClick={()=>handleInfoModal()}/></span>
+
+          </div>
+      </div>:null}
+      <div className={css.container} style={{ backgroundImage: `url(${image})` }}>
+        <div className={css.layout}>
+          <div className={css.iconContainer} onMouseEnter={()=>handleIcons()} onMouseLeave={()=>handleIcons()} >
+              {icons && <a className={css.link} target="_blank" href={deployLink}>
+                  <h1 className={css.icon}><FaExternalLinkAlt size='30px'/></h1>
+              </a>}
+              
+            {icons && <a className={css.link} target="_blank" href={gitHubLink}>
+                  <h1 className={css.icon}><SiGithub size='30px'/></h1>
+              </a>}
+
+              {icons && <a className={css.link} target="_blank" onClick={()=>handleInfoModal()}>
+                  <h1 className={css.icon}><BsInfoLg size='30px'/></h1>
+              </a>}
+          </div>
         </div>
+        
       </div>
-      
     </div>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,11 +3,9 @@ import css from "./Home.module.css";
 import img from "../Img/gitgub4.jpeg";
 import Cards from "../Cards/Cards";
 import Technologies from "../Technologies/Technologies";
-import {SiLinkedin, SiGithub} from 'react-icons/si'
-import {FaFilePdf} from 'react-icons/fa'
-import {MdLanguage} from 'react-icons/md'
-
-
+import { SiLinkedin, SiGithub } from "react-icons/si";
+import { FaFilePdf } from "react-icons/fa";
+import { MdLanguage } from "react-icons/md";
 
 function Home() {
   const [projects] = useState([
@@ -16,12 +14,40 @@ function Home() {
       name: "Ecommerce-App",
       gitHubLink: "https://github.com/CasanovaRamiro/ecommerce-pf",
       deployLink: "https://clothes-22.vercel.app",
+      infoEspañol: [
+        "Diseñar y desarrollar en un equipo conformado por un total de 7 integrantes un E-Commerce de indumentaria masculina y femenina que incluía: carrito de compras, plataforma de pago Mercado Pago, perfil del usuario, perfil de administrador con permisos especiales, historial de compras/ventas, descarga de órdenes de compra en PDF, búsquedas por parámetros, filtrados por categorías, ordenamientos y creación de nuevos productos.",
+        "Desarrollar la app usando para el Front-End React, Redux, CSS puro y Back-End desarrollado en Node.js con Express. Base de datos en PostgreSQL y Sequelize.",
+        "Complementar funcionalidades en colaboración de otros colegas para el área de Front-End.",
+        "Aportar funcionalidades destacadas: Local Storage (para persistir los datos de la sesión en curso y/o el dispositivo que se esté utilizando), control de Stock y manejo del carrito de compras.",
+        "Cooperar en Deploy del proyecto usando para Back-End Heroku CLI y para Front-End con Vercel",
+      ],
+      infoEnglish: [
+        "Design and develop an E-Commerce in a team of seven members. The app`s purpose was to sell men´s and women´s clothing. it included: shopping cart, Mercado Pago payment platform, user profile, administrator profile with special permissions, purchase history /sales, download of purchase invoices in PDF, search by parameters, filtering by categories, ordering and creation of new products.",
+        "The app was developed using React, Redux, pure CSS for the Front-End and the Back-End was developed in Node.js with Express, PostgreSQL and Sequelize.",
+        "I implemented functionalities in collaboration with other colleagues for the Front-End section.",
+        "I Provided outstanding functionalities: Local Storage (to save the data of the current session and/or the device being used), Stock control and shopping cart management.",
+        "I Cooperated in the Deploy of the project using Heroku CLI for Back-End and Vercel for Front-End.",
+      ],
     },
     {
       img: "dogs",
       name: "Dogs-App",
       gitHubLink: "https://github.com/CasanovaRamiro/Dogs",
       deployLink: "https://dogs-lilac-nu.vercel.app",
+      infoEnglish: [
+        "Develop a fullstack app to display information about the most popular dog breeds.",
+        "Fetch data from The Dog API.",
+        "Dynamic pagination and filtering.",
+        "Possibility to create, update and delete dog breeds and save it to a database through a validated form.",
+        "Tech Stack: React, Express, sequelize and PostgreSQL",
+      ],
+      infoEspañol: [
+        "Programar una app Full-Stack que muestre información detallada sobre razas populares de perros.",
+        "Buscar datos de The Dog API.",
+        "Paginación y Filtrado dinámicos.",
+        "Posibilidad de crear, modificar y eliminar razas de perros, guardándolas en una base de datos a través de un formulario validado",
+        "Tecnologías utilizadas: React, Express, sequelize y PostgreSQL",
+      ],
     },
   ]);
   const [spanish] = useState({
@@ -40,8 +66,10 @@ function Home() {
   return (
     <div className={css.container}>
       <div onClick={() => handleTranslate()} className={css.translateButton}>
-        <span><MdLanguage size='30px'/></span>
-        <h4>{language === 'EN'? 'English': 'Español'}</h4>
+        <span>
+          <MdLanguage size="30px" />
+        </span>
+        <h4>{language === "EN" ? "English" : "Español"}</h4>
       </div>
       <div className={css.titleImg}>
         <div className={css.imgContainer}>
@@ -49,26 +77,48 @@ function Home() {
             <img src={img} alt="Profile Pic" />
           </div>
           <div className={css.iconContainer}>
-          <span><a style={{ textDecoration: "none", color: 'rgb(255, 255, 255)'}} target="_blank" href='https://www.linkedin.com/in/ramiro-casanova'><SiLinkedin size={'30px'}/></a></span>
-          <span><a style={{ textDecoration: "none", color: 'rgb(255, 255, 255)'}} target="_blank" href='https://www.linkedin.com/in/ramiro-casanova'><SiGithub size={'30px'}/></a></span>
-          <span><a style={{ textDecoration: "none", color: 'rgb(255, 255, 255)'}} target="_blank" href='https://www.linkedin.com/in/ramiro-casanova'><FaFilePdf size={'30px'}/></a></span>
+            <span>
+              <a
+                style={{ textDecoration: "none", color: "rgb(255, 255, 255)" }}
+                target="_blank"
+                href="https://www.linkedin.com/in/ramiro-casanova"
+              >
+                <SiLinkedin size={"30px"} />
+              </a>
+            </span>
+            <span>
+              <a
+                style={{ textDecoration: "none", color: "rgb(255, 255, 255)" }}
+                target="_blank"
+                href="https://github.com/CasanovaRamiro"
+              >
+                <SiGithub size={"30px"} />
+              </a>
+            </span>
+            <span>
+              <a
+                style={{ textDecoration: "none", color: "rgb(255, 255, 255)" }}
+                target="_blank"
+                href="https://www.linkedin.com/in/ramiro-casanova"
+              >
+                <FaFilePdf size={"30px"} />
+              </a>
+            </span>
           </div>
         </div>
-         
+
         <div className={css.title}>
           <div className={css.titleSubTitle}>
             <h1>Ramiro Casanova</h1>
             <h4>
-              
               {language === "EN"
                 ? "Frontend developer based in Buenos Aires, Argentina"
                 : spanish.subTitle}
             </h4>
           </div>
         </div>
-       
       </div>
-      <h4 className={css.description} >
+      <h4 className={css.description}>
         {language === "EN"
           ? "Passionate about web development. Always learning new technologies. Here you will see some of projects."
           : spanish.description}
@@ -84,16 +134,17 @@ function Home() {
                 name={e.name}
                 gitHubLink={e.gitHubLink}
                 deployLink={e.deployLink}
+                infoEspañol={e.infoEspañol}
+                infoEnglish={e.infoEnglish}
+                language={language}
               />
             );
           })}
         </div>
       </div>
       <div className={css.technologies}>
-      <h2>{language === "EN" ? "Technologies" : "Tecnologias"}</h2>
-          <Technologies
-          language = {language}
-          />
+        <h2>{language === "EN" ? "Technologies" : "Tecnologias"}</h2>
+        <Technologies language={language} />
       </div>
     </div>
   );
