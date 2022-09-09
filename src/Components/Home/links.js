c20453873a0d20d071efacdc3521aa2810da27df
CasanovaRamiro <casanovaramiroj@gmail.com>
2022-09-09 10:52:35 -0300

    Updating and improving links

---
+++ src/Components/Home/links.js
@@ -0,0 +1,3 @@
+export const linkedIn = "https://www.linkedin.com/in/ramiro-casanova";
+export const CvPdfSpanish = "https://drive.google.com/file/d/14tSVGMXM6Sl_25edH0FK9kyuD62bmWed/view?usp=sharing";
+export const CvPdfEnglish = 'https://drive.google.com/file/d/1uVoocUmeKEmoQctjTxCcuri8vHmZ1is8/view?usp=sharing';
